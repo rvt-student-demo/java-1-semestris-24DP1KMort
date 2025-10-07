c9dece24b81828faf9ed0004c7e18fe7c829911d
lilMorty18 <kmortuzans58@gmail.com>
2025-10-07 16:42:23 +0300

    Gives average of two numbers.

---
+++ src/main/java/rvt/Average.java
@@ -0,0 +1,14 @@
+package rvt;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner (System.in);
+        System.out.println("Give the first number: ");
+        int num1 = scanner.nextInt();
+        Scanner scanner = new scanner (System.in);
+        System.out.println("Give the first number: ");
+        int num2 = scanner.nextInt();
+        System.out.println("The average is " + (num1 + num2) / 2.0);
+
+    }
+}
