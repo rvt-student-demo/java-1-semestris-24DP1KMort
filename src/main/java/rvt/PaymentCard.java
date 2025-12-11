bdaff95d9b74b1d7e1efdea26863743cd0b1baae
lilMorty18 <kmortuzans58@gmail.com>
2025-12-11 17:09:56 +0200

    PaymentCard

---
+++ src/main/java/rvt/PaymentCard.java
@@ -0,0 +1,32 @@
+package rvt;
+
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+            this.balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            this.balance -= 4.60;
+        }
+    }
+
+    public void addMoney(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            if (this.balance > 150) this.balance = 150;
+        }
+    }
+
+    public String toString() {
+        return "The card has " + this.balance + " euros";
+    }
+}
