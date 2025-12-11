0f4da48055801fe8c8ee4f11b94baca2f7af85ed
lilMorty18 <kmortuzans58@gmail.com>
2025-12-11 17:21:05 +0200

    Person

---
+++ src/main/java/rvt/Person.java
@@ -0,0 +1,15 @@
+package rvt;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return this.name + ", age " + this.age;
+    }
+}
